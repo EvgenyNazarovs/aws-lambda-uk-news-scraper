41f17087274ed0a7bb234b24ecd867f402a8a0dc
Eugene Nazarovs <eugene.nazarovs@gmail.com>
2021-04-30 17:58:18 +0100

    adds sam config

---
+++ .aws-sam/build/GuardianScraper/src/handlers/guardianScraper.js
@@ -0,0 +1,176 @@
+const dayjs = require('dayjs');
+const pLimit = require('p-limit');
+const { createItem } = require('/opt/dynamodbHelper')
+const { withBrowser, withPage } = require('/opt/scraperHelper');
+
+const calendar = {
+  jan: 1,
+  feb: 2,
+  mar: 3,
+  apr: 4,
+  may: 5,
+  jun: 6,
+  jul: 7,
+  aug: 8,
+  sep: 9,
+  oct: 10,
+  nov: 11,
+  dec: 12,
+};
+
+const { GuardianUrl, NewsScraperTable } = process.env;
+
+const limit = pLimit(10);
+
+let totalEval = 0;
+
+exports.handler = async () => {
+  try {
+    const articlesData = await withBrowser(async browser => {
+      const articleLinks = await getLinks(browser);
+      return Promise.all(articleLinks.map(async link => {
+        return getArticleData(browser, link)
+        }))
+    });
+
+    const articles = articlesData.map(processArticle);
+
+    const result = await Promise.all(articles.map(async article => {
+      console.log('article: ', article);
+      // return createItem(NewsScraperTable, article);
+    }))
+
+    console.log('result: ', result);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify('Articles Processed Successfully')
+    }
+    
+  } catch (err) {
+    console.error(err);
+    throw Error(err);
+  }
+}
+
+const getHeadlines = () => {
+  return [
+    ...document.getElementsByClassName(
+      "u-faux-block-link__overlay js-headline-text"
+      ),
+         ].map(item => item.getAttribute('href'))
+};
+
+const getLinks = async browser => {
+  try {
+    return withPage(browser)(async page => {
+      await page.goto(GuardianUrl, { waitUntil: 'networkidle2' });
+      return page.evaluate(getHeadlines)
+    });
+  } catch (err) {
+    console.error(err);
+    throw Error(err);
+  }
+}
+
+const processArticle = ([
+  author,
+  contentType,
+  keywords,
+  sectionName,
+  headline,
+  byline,
+  contentId,
+  thumbnail,
+  publication,
+  series,
+  seriesId,
+]) => {
+  if (contentType.toLowerCase() !== 'tag') {
+    const tags = keywords.split(',');
+    const newspaper = 'guardian';
+    const primaryKey = `${newspaper}`;
+    const {sortKey, section, title} = generateSortKeyParts(contentType, contentId);
+    const scrapedOn = dayjs().format('YYYY-MM-DDTHH:mm:ss.SSSZ');
+
+    return {
+      contentType,
+      tags,
+      sectionName,
+      headline,
+      contentId,
+      thumbnail,
+      publication,
+      sortKey,
+      primaryKey,
+      scrapedOn,
+      newspaper,
+      section,
+      title,
+      author: author || byline,
+      ...(series && { series }),
+      ...(seriesId && { seriesId })
+    }
+  }
+}
+
+const getArticleData = async (browser, link) => {
+  try {
+    return limit(() => withPage(browser)(async page => {
+      await page.goto(link);
+      totalEval += 1;
+      console.log(`evaluating ${link}, no ${totalEval}`);
+      return page.evaluate(() => {
+        const {
+          author,
+          contentType,
+          keywords,
+          sectionName,
+          headline,
+          byline,
+          contentId,
+          thumbnail,
+          publication,
+          series,
+          seriesId,
+        } = window.guardian.config.page;
+
+        return [
+          author,
+          contentType,
+          keywords,
+          sectionName,
+          headline,
+          byline,
+          contentId,
+          thumbnail,
+          publication,
+          contentId,
+          series,
+          seriesId,
+        ];
+      });
+    }))
+  } catch (err) {
+    console.error(err);
+    throw Error(err);
+  }
+}
+
+const generateSortKeyParts = (contentType, contentId) => {
+  if (contentType.toLowerCase() === 'article') {
+    const [section, y, m, d, title] = contentId.split('/');
+    return {
+      section,
+      title,
+      sortKey: `${section}#${y}-${calendar[m]}-${d}#${title}`
+    };
+  } else {
+    const [section,, y, m, d, title] = contentId.split('/');
+    return {
+      section,
+      title,
+      sortKey: `${section}#${y}-${calendar[m]}-${d}#${title}`
+    };
+  }
+}
---
+++ .aws-sam/build/GuardianScraper/src/utilitiesLayer/dynamodbHelper.js
@@ -0,0 +1,92 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+// const reservedWords = [
+//   'section'
+// ]
+
+const createItem = async (tableName, item) => {
+  try {
+    const params = {
+      TableName: tableName,
+      Item: item,
+      ReturnValues: 'ALL_NEW'
+    };
+
+    const { Attributes } = await documentClient.put(params).promise();
+
+    return Attributes;
+
+  } catch (err) {
+    console.error(err);
+    throw Error(err);
+  }
+}
+
+const getItem = async (tableName, primaryKey, sortKey) => {
+  try {
+    const params = {
+      TableName: tableName,
+      Key: {
+        "sortKey": sortKey,
+        "primaryKey": primaryKey
+      }
+    }
+
+    const { Item } = await documentClient.getItem(params).promise();
+
+    return Item;
+
+  } catch (err) {
+    console.error(err);
+    throw Error(err);
+  }
+}
+
+const deleteItem = async (tableName, primaryKey, sortKey) => {
+  try {
+    const params = {
+      TableName: tableName,
+      Key: {
+        'primaryKey': primaryKey,
+        'sortKey': sortKey
+      }
+    }
+
+    await documentClient.delete(params).promise();
+
+  } catch (err) {
+    console.error(err);
+    throw Error(err);
+  }
+}
+
+module.exports = {
+  createItem,
+  getItem,
+  deleteItem
+}
+
+// const updateItem = async (tableName, ) => {
+//   try {
+
+//   } catch (err) {
+//     console.error(err);
+//     throw Error(err);
+//   }
+// }
+
+// const getExpressionAttributeValues = item => Object.entries(item).reduce((obj, [key, value]) => ({...obj, [`:${key}`]: value}), {});
+
+// const getExpressionAttributeNames = item => (
+//   Object.keys(item).reduce((obj, key) => reservedWords.includes(key) ? {...obj, [`#${key}`]: `${key}`} : obj, {})
+// );
+
+// const getUpdateExpression = (item) => {
+//   return Object.keys(item).reduce((string, key, index) => {
+//     const expression = reservedWords.includes(key)
+//                      ? `#${key} = :${key}`
+//                      : `${key} = :${key}`;
+//     return index === 0 ? `${string} ${expression}` : `${string}, ${expression}`;
+//   }, "set");
+// };
