9766fe7735a547fa1e21b3ca3f774153065b476e
Eugene Nazarovs <eugene.nazarovs@gmail.com>
2021-04-30 15:02:12 +0100

    adds scraper utils

--- src/handlers/guardianScraper.js
+++ src/handlers/guardianScraper.js
@@ -3,6 +3,21 @@ const chromium = require('chrome-aws-lambda');
 const fs = require('fs');
 const documentClient = new AWS.DynamoDB.DocumentClient();
 
+const calendar = {
+  jan: 1,
+  feb: 2,
+  mar: 3,
+  apr: 4,
+  may: 5,
+  jun: 6,
+  jul: 7,
+  aug: 8,
+  sep: 9,
+  oct: 10,
+  nov: 11,
+  dec: 12,
+};
+
 const { GuardianUrl } = process.env;
 
 exports.handler = async () => {
@@ -25,9 +40,108 @@ exports.handler = async () => {
 
     console.log('article links: ', articleLinks);
 
+    let articlesData = [];
+
+    for (const link of articleLinks) {
+      await page.goto(link);
+      const result = await page.evaluate(() => {
+        const {
+          author,
+          contentType,
+          keywords,
+          sectionName,
+          headline,
+          byline,
+          contentId,
+          thumbnail,
+          publication,
+          series,
+          seriesId,
+        } = window.guardian.config.page;
+
+        return [
+          author,
+          contentType,
+          keywords,
+          sectionName,
+          headline,
+          byline,
+          contentId,
+          thumbnail,
+          publication,
+          series,
+          seriesId,
+        ];
+
+      })
+
+      articlesData.push(result);
+
+    }
+
+    await browser.close();
+
+    const articles = articlesData.map(([
+      author,
+      contentType,
+      keywords,
+      sectionName,
+      headline,
+      byline,
+      contentId,
+      thumbnail,
+      publication,
+      series,
+      seriesId,
+    ]) => {
+      if (contentType.toLowerCase() !== "tag") {
+        const tags = keywords.split(",");
+
+        const newspaper = "guardian";
+        const pk = `${contentType.toLowerCase()}#${newspaper}`;
+
+        let sk = "";
+
+        if (contentType.toLowerCase() === "article") {
+          const [sec, y, m, d, title] = contentId.split("/");
+          sk = `${sec}#${y}-${calendar[m]}-${d}#${title}`;
+        } else {
+          const [sec, type, y, m, d, title] = contentId.split("/");
+          sk = `${sec}#${y}-${calendar[m]}-${d}#${title}`;
+        }
+
+        const now = Date.now();
+        const today = new Date(now);
+        const scrapedOn = today.toISOString();
+
+        console.log("pk: ", pk);
+        console.log("sk: ", sk);
+
+        return {
+          contentType,
+          tags,
+          sectionName,
+          headline,
+          contentId,
+          thumbnail,
+          publication,
+          author: author || byline,
+          pk,
+          sk,
+          scrapedOn,
+          ...(series && { series }),
+          ...(seriesId && { seriesId }),
+        }
+      }
+    }).filter(article => article?.sk && article?.pk);
+
+    console.log('articles: ', articles);
+
+
     // const promises = articleLinks.map(async link => {
-    //   await page.goto(link);
-    //   return page.evaluate(() => {
+    //   const newPage = await browser.newPage();
+    //   await newPage.goto(link);
+    //   return newPage.evaluate(() => {
     //     const {
     //       author,
     //       contentType,
@@ -65,7 +179,9 @@ exports.handler = async () => {
     // })
 
     // const scraperData = await Promise.all(promises);
-    // await browser.close();
+
+    
+    
   } catch (err) {
     console.error(err);
     throw Error(err);
---
+++ src/util/scraperUtil.js
@@ -0,0 +1,28 @@
+const chromium = require('chrome-aws-lambda');
+
+const withBrowser = async fn => {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: true,
+  });
+  try {
+    return await fn(browser);
+  } finally {
+    await browser.close();
+  }
+}
+
+const withPage = browser => async fn => {
+  const page = await browser.newPage();
+  try {
+    return await fn(page);
+  } finally {
+    await page.close();
+  }
+}
+
+module.exports = {
+  withBrowser,
+  withPage
+}
